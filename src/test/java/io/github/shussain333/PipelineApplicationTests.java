72de6b91a6ddeaba84ea3c287199b0b0e4cfb3e0
shussain333 <sartajhussain3786@gmail.com>
2024-10-13 21:57:45 +0530

    pipeline setup

---
+++ src/test/java/io/github/shussain333/PipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.shussain333;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
